83bf7e2b524cf8124965fbb7579b78bc494e0d8c
YongJoon Joe <developer@yongjoon.net>
2018-12-07 16:25:27 +0900

    Implement getFileName and etc

--- src/main/kotlin/util/StringManager.kt
+++ src/main/kotlin/util/StringManager.kt
@@ -2,7 +2,25 @@ package util
 
 import java.io.File
 
+import directoryDelimiter
+
 fun generateStringFromFileList (strings : List<File>): String {
-    val internalString = strings.joinToString(separator = "\n")
+    val internalString = strings.map{getFullName(it.toString())}.joinToString(separator = "\n")
     return arrayOf("<\n", internalString, "\n>").joinToString(separator = "")
+}
+
+fun getFullName(path: String): String {
+    return path.toString().substringAfterLast(directoryDelimiter)
+}
+
+fun getFileName(path: String): String {
+    return path.toString().substringAfterLast(directoryDelimiter).substringBeforeLast(".")
+}
+
+fun getExtension(path: String): String {
+    return path.toString().substringAfterLast(directoryDelimiter).substringAfterLast(".")
+}
+
+fun getDirectory(path: String): String {
+    return path.toString().substringBeforeLast(directoryDelimiter)
 }
