c434fe6bab55c4e6068ca76c7f31c7643e126b3a
YongJoon Joe <developer@quietjoon.net>
2018-12-08 22:51:59 +0900

    Use else if

--- src/main/kotlin/AppLogic.kt
+++ src/main/kotlin/AppLogic.kt
@@ -4,10 +4,11 @@ fun getFirstOrSingleArchivePaths(paths: Array<String>) : Array<String> {
     var firstOrSingle: MutableList<String> = mutableListOf()
     for ( aPath in paths ) {
         if ( aPath.isArchive() ) {
+            // I knew this can be replaced by single if by using `maybePartNumber`
+            // But I want to leave this structure for easy reading
             if (aPath.isSingleVolume()) {
                 firstOrSingle.add(aPath)
-            }
-            if (aPath.isFirstVolume()) {
+            } else if (aPath.isFirstVolume()) {
                 firstOrSingle.add(aPath)
             }
         }
