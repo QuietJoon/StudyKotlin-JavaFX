bde9f6377508c5591b483f4ad98209afd50093bd
YongJoon Joe <developer@quietjoon.net>
2018-12-26 15:55:36 +0900

    Remove sleep code

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -6,17 +6,4 @@ fun main(args : Array<String>) {
     println("JavaFX")
     if (jBindingChecker()) Application.launch(GUI().javaClass, *args)
     println("End")
-
-    var count = 0
-    var lcount = 4
-    while (lcount>0) {
-        Thread.sleep(10L)
-        if (count < 100) {
-            count++
-        } else {
-            println(lcount)
-            count = 0
-            lcount -= 1
-        }
-    }
 }
